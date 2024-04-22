57229b691a149bf8c5a06fbcc88764a03b85928d
2432903 <seanlussier1331@gmail.com>
2024-04-22 14:24:11 -0400

    Updated the main() method printing message

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("This is a toy project");
     }
 }
